aa8c6073a5371f2b26ffbb5d310bd11b7bef59ee
CJ.Sandor <71267879+123csandor@users.noreply.github.com>
2022-03-10 20:35:39 -0500

    Add files via upload
    nothing to mention...

--- js/script.js
+++ js/script.js
@@ -137,3 +137,11 @@ document.addEventListener('DOMContentLoaded',function(event){
   }
   StartTextAnimation(0);
 });
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
